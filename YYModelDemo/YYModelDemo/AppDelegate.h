fcd523e65701ff1c1998b9fd51ff861330635415
xuweijie <ytl_sdk@126.com>
2018-07-16 10:44:50 +0800

    init proj xcode

---
+++ YYModelDemo/YYModelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYModelDemo
+//
+//  Created by ZHG on 2018/7/16.
+//  Copyright © 2018年 XWJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYModelDemo/YYModelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYModelDemo
+//
+//  Created by ZHG on 2018/7/16.
+//  Copyright © 2018年 XWJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
